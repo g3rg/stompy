49beb09f4452a38d73e6de19d0081fe4aae4c941
Greg <mauve.simian@gmail.com>
2023-02-11 22:36:01 +1000

    Non delay oscillating notes, serial text working

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 const int channel = 1;
 
 const int led = LED_BUILTIN;
+int ledState = LOW;
+unsigned long previousMillis = 0;
+const long interval = 250;
 
 Bounce button1 = Bounce(1, 5);  // 5 = 5 ms debounce time
 Bounce button2 = Bounce(2, 5);  // 5 = 5 ms debounce time
@@ -12,16 +15,36 @@ void setup() {
   pinMode(2, INPUT_PULLUP);
   pinMode(led, OUTPUT);
   usbMIDI.begin();
+
+  Serial.begin(9600);
+  digitalWrite(led, HIGH);
+  delay(5000);
+  digitalWrite(led, LOW);
+  Serial.println("Stompy Online");
+  delay(5000);
 }
 
 void loop() {
 
-  digitalWrite(led, HIGH);
-  usbMIDI.sendNoteOn(70, 99, channel);
-  delay(500);
-  digitalWrite(led, LOW);
-  usbMIDI.sendNoteOff(70, 99, channel);
-  delay(500);
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+      usbMIDI.sendNoteOn(60, 99, channel);
+    } else {
+      ledState = LOW;
+      usbMIDI.sendNoteOff(60, 99, channel);
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(led, ledState);
+  }
+
 
   button1.update();
   button2.update();
@@ -39,4 +62,5 @@ void loop() {
     if (button2.risingEdge()) {
     usbMIDI.sendNoteOff(67, 99, channel);  // 67 = C?
   }
+  
 }
